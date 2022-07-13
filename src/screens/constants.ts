ca3b61e2bf337bd7df693cdb998201e6e13094c1
James White <james.white@a-c02fm8luml7h.home>
2022-07-13 16:52:16 -0400

    new menu constants

---
+++ src/screens/constants.ts
@@ -0,0 +1,38 @@
+import { MenuItem } from '../shared/SideMenu'
+
+export const homeItems: MenuItem[] = [
+    {
+        link: "link",
+        title: "Journey"
+    },
+    {
+        link: "link",
+        title: "IAA"
+    },
+    {
+        link: "link",
+        title: "Message Center"
+    },
+    {
+        link: "link",
+        title: "Push Notifications"
+    }
+]
+export const categoryItems: MenuItem[] = [
+    {
+        link: "link",
+        title: "journey"
+    },
+    {
+        link: "link",
+        title: "iaa"
+    },
+    {
+        link: "link",
+        title: "message center"
+    },
+    {
+        link: "link",
+        title: "push notifications"
+    }
+]
